32f642544c4699678ad44559f8bc1f922570e0c5
Izaias Valentim <izaias451@gmail.com>
2024-03-03 14:17:43 -0300

    feat/criado os métodos de Login e Logout do usuário.

--- usuarios/api/views.py
+++ usuarios/api/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth import login, logout
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .serilizers import UsuarioSerializer
+from .serilizers import UsuarioSerializer, LoginSerializer
 from ..models import Usuario
 
 
@@ -12,5 +17,29 @@ class UsuarioViewSet(viewsets.ModelViewSet):
 
     http_method_names = ['get', 'post', ]
 
+
+class LoginUsuario(APIView):
+    permission_classes = [AllowAny, ]
+    authentication_classes = [SessionAuthentication, ]
+
+    def post(self, request):
+        data = request.data
+        serializer_login = LoginSerializer(data=data)
+        if serializer_login.is_valid(raise_exception=True):
+            usuario = serializer_login.auth_usuario(data=data)
+            login(request, usuario)
+            return Response({'Logado': usuario.username}, status=status.HTTP_200_OK)
+
+
+class LogoutUsuario(APIView):
+    permission_classes = [AllowAny, ]
+
+    def post(self, request):
+        usuario = request.user
+        if usuario is not None:
+            logout(request)
+            return Response({'Usuario deslogado': usuario.username}, status=status.HTTP_200_OK)
+        else:
+            return Response({'Inválido': 'Não há uma sessão ativa'}, status=status.HTTP_200_OK)
 
 
